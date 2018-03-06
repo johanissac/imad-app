ed04b4872033e9078cf0aae7057e7afdc7e64d92
johanissac <35626275+johanissac@users.noreply.github.com>
2018-03-06 22:24:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var pool=require('path').Pool;
-var config={
+/*var config={
     user:'johanissac',
     database:'johanisac',
     host:'db.imad.hasura-app.io',
     port:'5432',
     password:'process.env.DB_PASSWORD'
     
-}
+}*/
 var app = express();
 app.use(morgan('combined'));
 
@@ -24,7 +24,7 @@ var articleone = {
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-var pool=new pool(config);
+/*var pool=new pool(config);
 app.get('/test-db',function(req,res){
     pool.query('SELECT * FROM test',function(err,result){
         if(err){
@@ -35,7 +35,7 @@ app.get('/test-db',function(req,res){
         }
     });
 });
-
+*/
 app.get('/article1', function (req, res) {
   res.send(createTemplate(articleone));
 });
